5273491aa3dbaab1dfc183aa7c52cf955f3ae717
zygis <zygimantas.slikas1999@gmail.com>
2024-10-09 16:37:39 +0300

    added custom policy net framework

--- main.py
+++ main.py
@@ -8,10 +8,13 @@ from typing import Any, Callable, Dict, Tuple, Union
 import cupy as cp
 import cv2
 import gymnasium as gym
+import gymnasium.spaces
 import mlflow
 import numpy as np
 import stable_baselines3 as sb3
 import torch
+import torch.nn as nn
+import torch.optim
 import tqdm
 import yaml
 from metadrive import MetaDriveEnv, SafeMetaDriveEnv
@@ -20,6 +23,8 @@ from metadrive.envs.base_env import BaseEnv
 from metadrive.policy.idm_policy import IDMPolicy
 from stable_baselines3 import PPO
 from stable_baselines3.common.logger import HumanOutputFormat, KVWriter, Logger
+from stable_baselines3.common.policies import ActorCriticPolicy
+from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 from stable_baselines3.common.vec_env import SubprocVecEnv
 
 
@@ -85,7 +90,7 @@ class DataCollector:
     def collect_frames(self) -> np.ndarray | cp.ndarray:
         frames = []
         seed = self.config["seed"]
-        total_samples = self.config["training"]["batch_size"]
+        total_samples = self.config["training"]["steps"]
         if True:
             env = self.create_env(seed)
             start_time = time.perf_counter()
@@ -130,6 +135,115 @@ class MLflowOutputFormat(KVWriter):
                     mlflow.log_metric(key, value, step)
 
 
+class CustomCombinedExtractor(BaseFeaturesExtractor):
+    def __init__(self, observation_space: gym.spaces.Dict):
+        # We do not know features-dim here before going over all the items,
+        # so put something dummy for now. PyTorch requires calling
+        # nn.Module.__init__ before adding modules
+        super().__init__(observation_space, features_dim=1)
+
+        extractors = {}
+
+        total_concat_size = 0
+        # We need to know size of the output of this extractor,
+        # so go over all the spaces and compute output feature sizes
+        for key, subspace in observation_space.spaces.items():
+            if key == "image":
+                # We will just downsample one channel of the image by 4x4 and flatten.
+                # Assume the image is single-channel (subspace.shape[0] == 0)
+                extractors[key] = nn.Sequential(nn.MaxPool2d(4), nn.Flatten())
+                total_concat_size += subspace.shape[1] // 4 * subspace.shape[2] // 4
+            elif key == "vector":
+                # Run through a simple MLP
+                extractors[key] = nn.Linear(subspace.shape[0], 16)
+                total_concat_size += 16
+
+        self.extractors = nn.ModuleDict(extractors)
+
+        # Update the features dim manually
+        self._features_dim = total_concat_size
+
+    def forward(self, observations) -> torch.Tensor:
+        encoded_tensor_list = []
+
+        # self.extractors contain nn.Modules that do all the processing.
+        for key, extractor in self.extractors.items():
+            encoded_tensor_list.append(extractor(observations[key]))
+        # Return a (B, self._features_dim) PyTorch tensor, where B is batch dimension.
+        return torch.cat(encoded_tensor_list, dim=1)
+
+class CustomNetwork(nn.Module):
+    """
+    Custom network for policy and value function.
+    It receives as input the features extracted by the features extractor.
+
+    :param feature_dim: dimension of the features extracted with the features_extractor (e.g. features from a CNN)
+    :param last_layer_dim_pi: (int) number of units for the last layer of the policy network
+    :param last_layer_dim_vf: (int) number of units for the last layer of the value network
+    """
+
+    def __init__(
+        self,
+        feature_dim: int,
+        last_layer_dim_pi: int = 64,
+        last_layer_dim_vf: int = 64,
+    ):
+        super().__init__()
+
+        # IMPORTANT:
+        # Save output dimensions, used to create the distributions
+        self.latent_dim_pi = last_layer_dim_pi
+        self.latent_dim_vf = last_layer_dim_vf
+
+        # Policy network
+        self.policy_net = nn.Sequential(
+            nn.Linear(feature_dim, last_layer_dim_pi), nn.ReLU()
+        )
+        # Value network
+        self.value_net = nn.Sequential(
+            nn.Linear(feature_dim, last_layer_dim_vf), nn.ReLU()
+        )
+
+    def forward(self, features: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        :return: (th.Tensor, th.Tensor) latent_policy, latent_value of the specified network.
+            If all layers are shared, then ``latent_policy == latent_value``
+        """
+        return self.forward_actor(features), self.forward_critic(features)
+
+    def forward_actor(self, features: torch.Tensor) -> torch.Tensor:
+        return self.policy_net(features)
+
+    def forward_critic(self, features: torch.Tensor) -> torch.Tensor:
+        return self.value_net(features)
+
+
+class CustomActorCriticPolicy(ActorCriticPolicy):
+    def __init__(
+        self,
+        observation_space: gymnasium.spaces.Space,
+        action_space: gymnasium.spaces.Space,
+        lr_schedule: Callable[[float], float],
+        *args,
+        **kwargs,
+    ):
+        super().__init__(
+            observation_space,
+            action_space,
+            lr_schedule,
+            ortho_init=False,
+            features_extractor_class=CustomCombinedExtractor,
+            features_extractor_kwargs={},
+            optimizer_class=torch.optim.AdamW,
+            optimizer_kwargs={},
+            # Pass remaining arguments to base class
+            *args,
+            **kwargs,
+        )
+
+    def _build_mlp_extractor(self) -> None:
+        self.mlp_extractor = CustomNetwork(self.features_dim)
+
 def main() -> None:
     config: dict = yaml.safe_load(open("configs/main.yaml", "r"))
     print(f"Cores count: {os.cpu_count()}")
@@ -138,34 +252,39 @@ def main() -> None:
     np.random.seed(config["seed"])
     collector = DataCollector(config)
     # frames = collector.collect_frames()
-
-    loggers = Logger(
-        folder=None,
-        output_formats=[HumanOutputFormat(sys.stdout), MLflowOutputFormat()],
-    )
-
+    
     model = PPO(
-        policy="MultiInputPolicy",
+        policy=CustomActorCriticPolicy,
         env=collector.create_env(),
-        learning_rate=1e-5,
-        n_steps=4096, #batch size, n_env*n_steps
-        batch_size=64, #minibatch size
-        n_epochs = 10, 
-        gamma = 0.99,
-        gae_lambda=0.95, 
+        learning_rate=float(config["algorithm"]["learning_rate"]),
+        n_steps=config["algorithm"]["batch_size"], #batch size, n_env*n_steps
+        batch_size=config["algorithm"]["minibatch_size"], #minibatch size
+        n_epochs = config["algorithm"]["n_epochs"], 
+        gamma = config["algorithm"]["gamma"],
+        gae_lambda=config["algorithm"]["gae_lambda"], 
         # clip_range=
         # clip_range_vf=
         # ent_coef=
         # vf_coef=
         # max_grad_norm=
-        stats_window_size=100,
+        # stats_window_size=100,
         seed=config["seed"],
         device= "cuda" if torch.cuda.is_available() else "cpu",
         verbose=1,
     )
+    print(model.policy)
+    
+    sys.exit(0)
+
+
+    loggers = Logger(
+        folder=None,
+        output_formats=[HumanOutputFormat(sys.stdout), MLflowOutputFormat()],
+    )
     model.set_logger(loggers)
     with mlflow.start_run():
-        model.learn(total_timesteps=1e5, log_interval=1, progress_bar=True)
+        model.learn(total_timesteps=float(config["training"]["steps"]),
+                     log_interval=1, progress_bar=True)
     model.save("policy_1")
 
 
