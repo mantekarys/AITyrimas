d14bd30ef8b28e2c69308e0e5f915e3b72903645
Justas <24777490+justas147@users.noreply.github.com>
2024-11-04 17:18:49 +0200

    Remove comments

--- experiments/visual-transformer/vit_extractor.py
+++ experiments/visual-transformer/vit_extractor.py
@@ -9,23 +9,21 @@ import gymnasium.spaces
 import torch.nn as nn
 
 class ViTFeatureExtractor(BaseFeaturesExtractor):
-    # 224 * 224 * 3 * 4
-    # image size 224 * 224, 3 channels, 4 in stack
     def __init__(self, observation_space: gym.spaces.Dict, features_dim: int = 1):
         super(ViTFeatureExtractor, self).__init__(observation_space, features_dim)
         print("Init ViTFeatureExtractor")
         print(observation_space)
         
-        # assert that hey exists in observation_space
+        # assert that image exists in observation_space
         assert "image" in observation_space.spaces, "Image observation is missing!"
         
         # Load a pre-trained Vision Transformer from timm
         self.vit = timm.create_model('vit_base_patch16_224', pretrained=True, num_classes=0)
         self.vit.eval()
     
-        subspaces = observation_space.spaces["image"].shape[1] // 4 * observation_space.spaces["image"].shape[2] // 4
-        print(subspaces)
-        # Ensure the transformer outputs the correct number of features
+        # subspaces = observation_space.spaces["image"].shape[1] // 4 * observation_space.spaces["image"].shape[2] // 4
+        # print(subspaces)
+
         print(self.vit.num_features)
         self._features_dim = self.vit.num_features
 
