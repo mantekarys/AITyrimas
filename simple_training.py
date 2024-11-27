5a2a764d4ce2f394489075b1185a8d7d03b6513e
zygis <zygimantas.slikas1999@gmail.com>
2024-11-28 01:54:29 +0200

    dinov2 model trained for 1M steps

--- simple_training.py
+++ simple_training.py
@@ -34,7 +34,7 @@ from cnn_custom_policy import CustomResNetPolicy
 
 # from custom_policy_1 import CustomCNNPolicy
 # from custom_policy_2 import CustomViTPolicy
-# from custom_policy_3 import CustomViTPolicy2
+from custom_policy_3 import CustomViTPolicy2
 
 
 class DataCollector:
@@ -73,7 +73,7 @@ class DataCollector:
                 "on_continuous_line_done": False,
             }
         )
-        env = utils.CNN_FixedSafeMetaDriveEnv(
+        env = utils.FixedSafeMetaDriveEnv(
             return_image=self.return_image, env_config=sim_config
         )
         return env
@@ -190,7 +190,7 @@ def main(config_file: str = "main.yaml", base_model: str | None = None) -> None:
     collector = DataCollector(config)
     parallel_envs = collector.create_env()
     model = PPO(
-        policy=CustomResNetPolicy,
+        policy=CustomViTPolicy2,
         env=parallel_envs,
         learning_rate=lr,
         n_steps=config["algorithm"]["batch_size"],  # batch size, n_env*n_steps
@@ -244,7 +244,7 @@ if __name__ == "__main__":
     # main("test_1.yaml", "models/upset-asp-587.zip")
     # test_policy("models/sincere-ape-126.zip", 2000)
     test_policy(
-        "models/chill-owl-867-big-cnn-4.zip", 2000, config="test_1.yaml", just_embeddings=False
+        "models/chill-owl-867-1M.zip", 2000, config="test_1.yaml", just_embeddings=True
     )
 
 
